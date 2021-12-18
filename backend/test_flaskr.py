85a532b656f6bd3c98a3c7f77602855d3f34ff4c
alf808 <alf@akademe.net>
2021-12-18 10:37:28 -1000

    skeletal tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -18,6 +18,13 @@ class FriviaTestCase(unittest.TestCase):
         self.database_path = "postgresql:///{}".format(self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.new_question = {
+            'question': 'What is the capital of Hawaii',
+            'answer': 'Honolulu',
+            'category': 3,
+            'difficulty': 1
+        }
+
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -33,6 +40,43 @@ class FriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        pass
+    def test_get_question_search_with_results(self):
+        pass
+    def test_get_question_search_without_results(self):
+        pass
+    def test_get_categories(self):
+        pass
+    def test_get_categories_non_exist(self):
+        pass
+    def test_get_questions_by_category(self):
+        pass
+    def test_404_get_questions_by_category(self):
+        pass
+    def test_create_question(self):
+        pass
+    def test_422_if_book_creation_fails(self):
+        pass
+    def test_delete_question(self):
+        pass
+    def test_422_delete_if_question_not_exist(self):
+        pass
+    def test_get_quiz(self):
+        pass
+    def test_422_get_quiz(self):
+        pass
+    def test_404_get_quiz_no_category(self):
+        pass
 
 
 # Make the tests conveniently executable
